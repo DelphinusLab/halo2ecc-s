2e29a7d040bafb4b567ab38e2a551be6aa6375d8
Heng Zhang <shindar90@gmail.com>
2024-03-04 23:49:26 +0800

    fix keccak

--- src/circuit/keccak_chip.rs
+++ src/circuit/keccak_chip.rs
@@ -13,12 +13,12 @@ pub const ABSORB_BITS_RATE: usize = 1088;
 
 pub type AssignedState<N> = [[[AssignedCondition<N>; W]; T]; T];
 
-const ROTATION_CONSTANTS: [[usize; 5]; 5] = [
-    [0, 36, 3, 41, 18],
-    [1, 44, 10, 45, 2],
-    [62, 6, 43, 15, 61],
-    [28, 55, 25, 21, 56],
-    [27, 20, 39, 8, 14],
+const RHO: [u32; 24] = [
+    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44,
+];
+
+const PI: [usize; 24] = [
+    10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1,
 ];
 
 pub const N_R: usize = T * T - 1;
@@ -61,8 +61,8 @@ pub trait KeccakChipOps<N: FieldExt> {
     fn theta(&mut self, state: &mut AssignedState<N>) {
         let mut c = state[0].clone();
 
-        let prev = |x| (x + 4) % 5;
-        let next = |x| (x + 1) % 5;
+        let prev = |x: usize| (x + 4) % 5;
+        let next = |x: usize| (x + 1) % 5;
 
         for x in 0..T {
             let y = &state[x];
@@ -74,7 +74,6 @@ pub trait KeccakChipOps<N: FieldExt> {
             }
             c[x] = ci;
         }
-
         for x in 0..T {
             let mut di = c[next(x)].clone();
             di.rotate_left(1);
@@ -89,24 +88,16 @@ pub trait KeccakChipOps<N: FieldExt> {
         }
     }
 
-    fn rho(&mut self, state: &mut AssignedState<N>) {
-        for x in 0..T {
-            for y in 0..T {
-                state[x][y].rotate_left(ROTATION_CONSTANTS[x][y]);
-            }
+    fn rho_and_pi(&mut self, state: &mut AssignedState<N>) {
+        let mut last = state[1][0].clone();
+        for i in 0..N_R {
+            let pi_x = PI[i] % 5;
+            let pi_y = PI[i] / 5;
+            let array = state[pi_x][pi_y].clone();
+            last.rotate_left(RHO[i] as usize);
+            state[pi_x][pi_y].copy_from_slice(&last[..]);
+            last = array;
         }
-    }
-
-    fn pi(&mut self, state: &mut AssignedState<N>) {
-        let mut out = state.clone();
-
-        for x in 0..T {
-            for y in 0..T {
-                out[y][(2 * x + 3 * y) % 5] = state[x][y].clone();
-            }
-        }
-
-        *state = out;
     }
 
     fn xi(&mut self, state: &mut AssignedState<N>) {
@@ -132,7 +123,8 @@ pub trait KeccakChipOps<N: FieldExt> {
     fn iota(&mut self, state: &mut AssignedState<N>, round: usize) {
         for z in 0..W {
             // xor_const(a, b) = if b is 0 { a } else { not(a) }
-            if ROUND_CONSTANTS[round] & (1 << z) != 0 {
+            // The state bit is BE
+            if ROUND_CONSTANTS[round] & (1 << (W - z - 1)) != 0 {
                 state[0][0][z] = self.base_chip().not(&state[0][0][z]);
             }
         }
@@ -141,8 +133,7 @@ pub trait KeccakChipOps<N: FieldExt> {
     fn permute(&mut self, state: &mut AssignedState<N>) {
         for i in 0..N_R {
             self.theta(state);
-            self.rho(state);
-            self.pi(state);
+            self.rho_and_pi(state);
             self.xi(state);
             self.iota(state, i);
         }
@@ -152,11 +143,18 @@ pub trait KeccakChipOps<N: FieldExt> {
         assert!(input.len() == ABSORB_BITS_RATE);
         let mut x = 0;
         let mut y = 0;
+        const W_BYTES: usize = W / 8;
         for i in 0..ABSORB_BITS_RATE / W {
-            for j in 0..W {
-                state[x][y][j] = self.base_chip().xor(&input[i * W + j], &state[x][y][j]);
+            for j in 0..W_BYTES {
+                for k in 0..8 {
+                    // caused by different endian
+                    let z = i * W + j * 8 + k;
+                    let permuted_z = (W_BYTES - j - 1) * 8 + k;
+                    state[x][y][permuted_z] =
+                        self.base_chip().xor(&input[z], &state[x][y][permuted_z]);
+                }
             }
-            if x < 5 - 1 {
+            if x < T - 1 {
                 x += 1;
             } else {
                 y += 1;
@@ -210,15 +208,15 @@ pub trait KeccakChipOps<N: FieldExt> {
         bits.try_into().unwrap()
     }
 
-    fn compose_u256_to_scalar_be(&mut self, s: &[AssignedCondition<N>]) -> AssignedValue<N> {
-        assert!(s.len() == 256);
+    fn compose_to_scalar_be(&mut self, s: &[AssignedCondition<N>]) -> AssignedValue<N> {
+        assert!(s.len() % 2 == 0);
         let mut acc = self.base_chip().assign_constant(N::zero());
 
         let one = N::one();
         let two = one + &one;
         let four = two + &two;
 
-        for i in 0..256 / 2 {
+        for i in 0..s.len() / 2 {
             let b0 = s[i * 2 + 1];
             let b1 = s[i * 2];
 
@@ -246,25 +244,25 @@ pub trait KeccakChipOps<N: FieldExt> {
         let padding_len = aligned_len - raw_len;
 
         if padding_len == 8 {
-            // padding 0x86
+            // padding 0x81
             input_bits.push(assigned_one.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
-            input_bits.push(assigned_one.clone());
-            input_bits.push(assigned_one.clone());
             input_bits.push(assigned_zero.clone());
+            input_bits.push(assigned_zero.clone());
+            input_bits.push(assigned_one.clone());
         } else {
-            // padding 0x06
+            // padding 0x01
+            input_bits.push(assigned_zero.clone());
+            input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_zero.clone());
             input_bits.push(assigned_one.clone());
-            input_bits.push(assigned_one.clone());
-            input_bits.push(assigned_zero.clone());
 
             // padding zero
             for _ in 0..padding_len - 16 {
@@ -286,8 +284,19 @@ pub trait KeccakChipOps<N: FieldExt> {
             self.absorb(&mut state, &c);
         }
 
-        let res_bits = vec![state[0][0], state[1][0], state[2][0], state[3][0]].concat();
-        self.compose_u256_to_scalar_be(&res_bits[..])
+        let res_bits = vec![state[0][0], state[1][0], state[2][0], state[3][0]]
+            .into_iter()
+            .map(|x| {
+                x.chunks_exact(8)
+                    .rev()
+                    .flatten()
+                    .cloned()
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+            .concat();
+
+        self.compose_to_scalar_be(&res_bits[..])
     }
 }
 
--- src/tests/keccak_chip.rs
+++ src/tests/keccak_chip.rs
@@ -5,34 +5,47 @@ use crate::tests::{random_fr, run_circuit_on_bn256};
 use ark_std::{end_timer, start_timer};
 use halo2_proofs::arithmetic::BaseExt;
 use halo2_proofs::pairing::bn256::Fr;
+use halo2_proofs::pairing::bn256::G1Affine;
+use halo2_proofs::transcript::Challenge255;
+use halo2_proofs::transcript::EncodedChallenge;
 use sha3::Digest;
 
 #[test]
 fn test_keccak_chip() {
-    let mut ctx = Context::<Fr>::new();
-    {
-        let timer = start_timer!(|| "setup");
-        let mut hasher = sha3::Keccak256::new();
-        let mut input = vec![];
-        for _ in 0..0 {
-            let v = random_fr();
+    for i in (0..16).step_by(3) {
+        let mut ctx = Context::<Fr>::new();
+        {
+            let timer = start_timer!(|| "setup");
+            let mut hasher = sha3::Keccak256::new();
+            let mut input = vec![];
+            for _ in 0..i {
+                let v = random_fr();
 
-            let mut buf = vec![];
-            v.write(&mut buf).unwrap();
-            buf.resize(32, 0u8);
-            buf.reverse();
-            hasher.update(buf);
+                let mut buf = vec![];
+                v.write(&mut buf).unwrap();
+                buf.resize(32, 0u8);
+                buf.reverse();
+                hasher.update(buf);
 
-            input.push(ctx.assign(v));
+                input.push(ctx.assign(v));
+            }
+
+            let assigned_hash = ctx.hash(&input[..]);
+            let mut bytes = hasher.finalize().as_slice().to_vec();
+            bytes.reverse();
+            bytes.resize(64, 0u8);
+            let expected_hash =
+                Challenge255::<G1Affine>::new(&bytes.try_into().unwrap()).get_scalar();
+            let assigned_expected_hash = ctx.assign(expected_hash);
+
+            ctx.assert_equal(&assigned_hash, &assigned_expected_hash);
+
+            //println!("assigned_hash {:?}", assigned_hash);
+            //println!("assigned_expected_hash {:?}", assigned_expected_hash);
+            //println!("ctx {} {}", ctx.base_offset, ctx.range_offset);
+
+            end_timer!(timer);
         }
-
-        let assigned_hash = ctx.hash(&input[..]);
-        let result: [u8; 32] = hasher.finalize().as_slice().try_into().unwrap();
-        println!("assigned_hash {:?}", assigned_hash);
-        println!("result {:?}", result);
-        println!("ctx {} {}", ctx.base_offset, ctx.range_offset);
-        end_timer!(timer);
+        run_circuit_on_bn256(ctx, 20);
     }
-
-    run_circuit_on_bn256(ctx, 20);
 }
